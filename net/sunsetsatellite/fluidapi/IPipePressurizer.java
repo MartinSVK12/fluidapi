f2e57e051c1a1d3c56b4adae3892d4d845cea9fc
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2023-01-22 12:28:38 +0100

    Remove modloader stuff

--- net/sunsetsatellite/fluidapi/IPipePressurizer.java
+++
@@ -1,9 +0,0 @@
-package net.sunsetsatellite.fluidapi;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface IPipePressurizer {
-    void pressurizePipes(TileEntityFluidPipe pipe, ArrayList<HashMap<String,Integer>> already);
-    void unpressurizePipes(TileEntityFluidPipe pipe,ArrayList<HashMap<String,Integer>> already);
-}
