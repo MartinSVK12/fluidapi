bc0a4a7d68be4f28a136fbbc580ec644ce6ae412
MartinSVK12 <martinmilesich2882@gmail.com>
2023-03-18 20:46:22 +0100

    Update Connection.java

--- src/main/java/sunsetsatellite/fluidapi/util/Connection.java
+++ src/main/java/sunsetsatellite/fluidapi/util/Connection.java
@@ -1,8 +1,18 @@
 package sunsetsatellite.fluidapi.util;
 
 public enum Connection {
-    INPUT,
-    OUTPUT,
-    BOTH,
-    NONE,
+    INPUT("I"),
+    OUTPUT("O"),
+    BOTH("B"),
+    NONE("X");
+
+    final String c;
+
+    Connection(String x) {
+        this.c = x;
+    }
+
+    public String getLetter() {
+        return c;
+    }
 }
